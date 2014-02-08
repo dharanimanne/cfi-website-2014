b29d3e8765769b4ecf8783791a111f49f2424169
Vineet <vineet@outlook.in>
2014-02-08 14:31:50 +0530

    Adding user, password class

---
+++ PMP/classes/Password.php
@@ -0,0 +1,25 @@
+<?php 
+	class Password {
+	 
+		//Blowfish
+		private static $algo = '$2a';
+	 
+		//Cost
+		private static $cost = '$10';
+	 	 
+		//Salt
+		private static $salt = '#e$54^&de%*(sd';
+	 
+		//Generate password hash
+		public static function hash( $password ){	 
+			return crypt( $password,	self::$algo . self::$cost . '$' . $salt );	 
+		}	 
+		
+		//Comapre hash and password
+		public static function validate_password($hash, $password) {	 
+			$full_salt = substr($hash, 0, 20);	 
+			$new_hash = crypt($password, $full_salt);	 
+			return ($hash == $new_hash);	 
+		}	 
+	}
+?>
---
+++ PMP/config.php
@@ -0,0 +1,20 @@
+﻿<?php
+ini_set( "display_errors", true );
+date_default_timezone_set( "Asia/Calcutta" );
+define( "DB_DSN", "mysql:host=localhost;dbname=pmp" );
+define( "DB_USERNAME", "username" );
+define( "DB_PASSWORD", "password" );
+define( "CLASS_PATH", "classes" );
+define( "TEMPLATE_PATH", "templates" );
+define( "ADMIN_USERNAME", "admin" );
+define( "ADMIN_PASSWORD", "pass" );
+define( "TABLENAME_USERS", "user" );
+require( CLASS_PATH . "/index.php" );
+ 
+function handleException( $exception ) {
+  echo "Sorry, a problem occurred. Please try later.";
+  error_log( $exception->getMessage() );
+}
+ 
+set_exception_handler( 'handleException' );
+?>
