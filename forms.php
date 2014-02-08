915c382793a0a5b688ee2458ddd5907c934d61bd
shivakumarpvm <pvmshiva596@gmail.com>
2014-02-08 16:21:54 +0530

    forms added

---
+++ forms.php
@@ -0,0 +1,36 @@
+<html>
+<head>
+	<title>forms</title>
+	<link type="text/css" rel="stylesheet" href="css/bootstrap.css"/>
+		<link type="text/css" rel="stylesheet" href="css/bootstrap.min.css"/>
+		<link type="text/css" rel="stylesheet" href="css/bootstrap-theme.css"/>
+		<link type="text/css" rel="stylesheet" href="css/bootstrap-theme.min.css"/>
+</head>
+<body class="well">
+	<form action="process.php" method="post" enctype="multipart/form-data">
+		<label>USER NAME:</label><br/>
+		<input type="text" class="span3" name="username" value=""/>
+		<br/>
+		<label>PASSWORD:</label><br/>
+		<input type="password" class="span3" name="password" value=""/>
+		<br/>
+		<label>NAME:</label><br/>
+		<input type="text" class="span3" name="name" value=""/>
+		<br/>
+		<label>ROLL NUMBER:</label><br/>
+		<input type="text" name="mm" value=""/>
+		<br/>
+		
+		<label>HOSTEL:</label><br/>
+		<input type="text" name="hostel" value=""/>
+		<br/>
+		<label>ROOM NUMBER:</label><br/>
+		<input type="text" name="me" value=""/>
+		<br/>
+		
+		<br/>
+		<button  class="btn btn-primary"/>SUBMIT</button>
+    </form>
+	<script src="js/bootstrap.js"></script>
+</body>
+</html>
