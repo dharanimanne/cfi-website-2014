08729b5ae17534b42c5bda628fa1074672ded841
vsvrp-1995 <vsvrp.1995@gmail.com>
2014-02-14 01:32:36 +0530

    mostly frontend

---
+++ PMP/templates/include/sidebar.php
@@ -0,0 +1,21 @@
+<div id='content' class='row-fluid' style="margin-top:50px;">
+      <div class='span3 sidebar'>
+ 
+<ul class="nav nav-list">  
+        <li class="nav-header">List header</li>  
+        <li class="active"><a href="#"><i class="icon-white icon-home"></i> Home</a></li>  
+        <li><a href="#"><i class="icon-book"></i> Library</a></li>  
+        <li><a href="#"><i class="icon-pencil"></i> Applications</a></li>  
+        <li class="nav-header">Another list header</li>  
+        <li><a href="#"><i class="icon-user"></i> Profile</a></li>  
+        <li><a href="#"><i class="icon-cog"></i> Settings</a></li>  
+        <li class="divider"></li>  
+        <li><a href="#"><i class="icon-flag"></i> Help</a></li>  
+</ul>
+     </div>
+
+
+      <div class='span9 main'>
+        <h2>Main Content Section</h2>
+      </div>
+</div>
