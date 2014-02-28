a581171d6011abd48a20af6bbd87a5c7222b6505
Dharani Manne <dharani.manne@gmail.com>
2014-02-28 12:54:43 +0530

    created file.php but havent finished it yet

--- PMP/index.php
+++ PMP/index.php
@@ -26,6 +26,9 @@
 			break;
 		case 'updatePassword';
 			updatePassword();
+			break;
+		case 'uploadFile';
+			uploadFile();
 			break;	
 		case 'add_activity';
             add_activity();		
@@ -95,53 +98,51 @@
 		$results = array();	
 		$results['pageTitle'] = "Login | CFI Projects Management Portal";	
 		$user = new User( $_POST );
-		if ( ! isset($_FILES["file"])) {
-    die('file is not set...');
-}
-else
-{
-		$photo = $_FILES["file"]["name"];
-$allowedExts = array("gif", "jpeg", "jpg", "png");
-  $temp = explode(".", $photo);
-  $extension = end($temp);
-  if ((($_FILES["file"]["type"] == "image/gif")
-  || ($_FILES["file"]["type"] == "image/jpeg")
-  || ($_FILES["file"]["type"] == "image/jpg")
-  || ($_FILES["file"]["type"] == "image/pjpeg")
-  || ($_FILES["file"]["type"] == "image/x-png")
-  || ($_FILES["file"]["type"] == "image/png"))
-  && ($_FILES["file"]["size"] < 20000000)
-  && in_array($extension, $allowedExts))
-  {
-  if ($_FILES["file"]["error"] > 0)
-    {
-    echo "Return Code: " . $_FILES["file"]["error"] . "<br>";
-    }
-  else
-    {
-  
-
-    if (file_exists("upload/" . $_FILES["file"]["name"]))
-      {
-      echo $_FILES["file"]["name"] . " already exists. ";
-      }
-    else
-      {
-	  $name1=$_FILES["file"]["name"];
-      move_uploaded_file($_FILES["file"]["tmp_name"],
-      "upload/" . $_FILES["file"]["name"]);
-      echo "Stored in: " . "upload/" . $_FILES["file"]["name"];
-	
-      }
-    }
-  }
-  else
-  {
-  echo "Invalid file";
-  }
-  }
-  $user->avatarLocation = $name1;
-		
+		if ( ! isset($_FILES["file"]))
+		{
+    		die('file is not set...');
+		}
+		else
+		{
+			$photo = $_FILES["file"]["name"];
+			$allowedExts = array("gif", "jpeg", "jpg", "png");
+			$temp = explode(".", $photo);
+		    $extension = end($temp);
+		  	if ((($_FILES["file"]["type"] == "image/gif")
+		  	|| ($_FILES["file"]["type"] == "image/jpeg")
+		  	|| ($_FILES["file"]["type"] == "image/jpg")
+		  	|| ($_FILES["file"]["type"] == "image/pjpeg")
+		  	|| ($_FILES["file"]["type"] == "image/x-png")
+		  	|| ($_FILES["file"]["type"] == "image/png"))
+		  	&& ($_FILES["file"]["size"] < 20000000)
+		  	&& in_array($extension, $allowedExts))
+		  	{
+		  		if ($_FILES["file"]["error"] > 0)
+		    	{
+		    		echo "Return Code: " . $_FILES["file"]["error"] . "<br>";
+		    	}
+		  		else
+		    	{
+		  			if (file_exists("upload/" . $_FILES["file"]["name"]))
+		      		{
+				    	echo $_FILES["file"]["name"] . " already exists. ";
+				    }
+		    		else
+		      		{
+						$name1=$_FILES["file"]["name"];
+				        move_uploaded_file($_FILES["file"]["tmp_name"],
+				        "upload/" . $_FILES["file"]["name"]);
+				        echo "Stored in: " . "upload/" . $_FILES["file"]["name"];
+					}
+		    	}
+		  	}
+		  	else
+		  	{
+		  		echo "Invalid file";
+		  	}
+		}
+		$user->avatarLocation = $name1;
+				
 		if( $user->insert() ){
 			$results['successMessage'] = "Registration successful. Please login.";
 		}		
@@ -157,9 +158,7 @@ $allowedExts = array("gif", "jpeg", "jpg", "png");
 				$results['errorMessage'] = "Registration unsuccessful, invalid roll no provided.";
 			else
 				$results['errorMessage'] = "Registration unsuccessful. Please try again.";
-		}
-		
-		
+		}	
 	}
 
 	function add_activity(){
