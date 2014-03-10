28634f2e1927e70d8db1f06efb9e0985a0514dd3
dharanimanne <dharani.manne@gmail.com>
2014-03-11 03:45:42 +0530

    form to add preferences to table done

--- PMP/preferences/preferences.php
+++ PMP/preferences/preferences.php
@@ -2,7 +2,7 @@
 	
 	if ( isset($_POST) ){
 			
-		$con=mysqli_connect(DB_USERNAME, DB_PASSWORD, DB_DSN); 
+		$con=mysqli_connect( 'localhost', DB_USERNAME, DB_PASSWORD, DB_DSN); 
 		// Check connection
 		if (mysqli_connect_errno())
 		  {
@@ -12,18 +12,12 @@
 		else {
 
 			$name = $_POST['name'];
-			$rollNo = $_POST['rollNo'];
-			$hostel = $_POST['hostel'];
-			$room = $_POST['room'];
-			$phone = $_POST['phone'];
-			$email = $_POST['email'];
-			$aboutMe = $_POST['aboutMe'];
 			$preference1 = $_POST['preference1'];
 			$preference2 = $_POST['preference2'];
 			$uploadedOn = date("Y-m-d H:i:s");
 		
-			$sql = "INSERT INTO preferences (name, rollNo, hostel, room, phone, email, aboutMe, preference1, preference2, uploadedOn)
-			VALUES ( ".$name.", ".$rollNo.", ".$hostel.", ".$room.", ".$phone.", ".$email.", ".$aboutMe.", ".$preference1.", ".$preference2.", ".$uploadedOn.")";
+			$sql = "INSERT INTO preferences (name, preference1, preference2, uploadedOn)
+			VALUES ( ".$name.", ".$preference1.", ".$preference2.", ".$uploadedOn.")";
 			if ( mysqli_query($con,$sql) )
 			{
 				echo "Successfully registered your preferences. Thank you.";
@@ -35,17 +29,3 @@
 	}
 
 ?>
-	
-		$con=mysqli_connect(DB_USERNAME, DB_PASSWORD, DB_DSN); 
-		// Check connection
-		if (mysqli_connect_errno())
-		  {
-		  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-		  }
-		else {
-		
-			$sql = "SELECT title FROM summerprojects WHERE category='".$category."'";
-			$result = mysqli_query($con,$sql);
-
-		mysqli_close($con);
-		}
