43950a1311529cfff93c27bd130cd63a865545b9
Vineet <vineet@outlook.in>
2014-03-10 12:15:48 +0530

    restructuring the repo

--- tuts/inserting2.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require('/../PMP/config.php');
-require ('/../PMP/classes/Activity.php');
-$details = array("title" => "title", "brief_writeup" => "brief", "detailed_writeup" => "this is the detailed writeup", "status" => "status", "tags" => "tags", "overall_budget" => 10000, "utilized_budget" => 15000, "activity_type" => "club", "icon_link" => "absent", "bg_image_link" => "absent");
-$dharani = new Activity($details);
-$dharani->insert();
-?>
