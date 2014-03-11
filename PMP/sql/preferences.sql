8c4b65f2b1b75f6e57f767806c6cefadc8fec0d1
razerboot <robotxtron2010@gmail.com>
2014-03-11 06:03:32 +0530

    in preferences.sql  changed the sequence

--- PMP/sql/preferences.sql
+++ PMP/sql/preferences.sql
@@ -2,16 +2,15 @@ DROP TABLE IF EXISTS preferences;
 CREATE TABLE preferences
 (
   id                smallint unsigned NOT NULL auto_increment,
+  name              VARCHAR(50) NOT NULL, 
+  rollNo            VARCHAR(30) NOT NULL, 
+  hostel            VARCHAR(20) NOT NULL, 
+  room              VARCHAR(20) NOT NULL, 
+  phone             VARCHAR(20) NOT NULL, 
+  email             VARCHAR(50) NOT NULL UNIQUE,
+  aboutMe           text NULL,
   preference1       VARCHAR(100) NOT NULL,
   preference2       VARCHAR(100) NOT NULL,
   uploadedOn        datetime NULL,
-  name              VARCHAR(50) NOT NULL,
-  rollNo            VARCHAR(30) NOT NULL,
-  hostel            VARCHAR(20) NOT NULL,
-  room              VARCHAR(20) NOT NULL,
-  phone             VARCHAR(20) NOT NULL,
-  email             VARCHAR(50) NOT NULL UNIQUE,
-  aboutMe           text NULL,
-
   PRIMARY KEY     (id)
 );
