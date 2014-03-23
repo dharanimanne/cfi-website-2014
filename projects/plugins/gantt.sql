b296f03863d3f16a2e76d4468eae094573fea7b0
razerboot <robotxtron2010@gmail.com>
2014-03-23 05:53:24 +0530

    database gantt added

--- projects/plugins/gantt.sql
+++ projects/plugins/gantt.sql
@@ -3,6 +3,7 @@ CREATE TABLE `cfi_gantt_links` (
   `source` int(11) NOT NULL,
   `target` int(11) NOT NULL,
   `type` varchar(1) NOT NULL,
+  `activity_id` smallint(5) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
 );
 CREATE TABLE `cfi_gantt_tasks` (
@@ -13,5 +14,6 @@ CREATE TABLE `cfi_gantt_tasks` (
   `progress` float NOT NULL,
   `sortorder` int(11) NOT NULL,
   `parent` int(11) NOT NULL,
+  `activity_id` smallint(5) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
 )
